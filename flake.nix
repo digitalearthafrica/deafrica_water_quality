b4d77ba89ff2dc59823a14951760fc35b25432b4
Victoria Neema <victoria@kartoza.com>
2026-02-20 22:08:20 +0300

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Water quality python environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11"; # Updated to match your other config
     micromamba-shell.url = "github:vikineema/micromamba-shell";
   };
 
@@ -14,34 +14,48 @@
       ...
     }@inputs:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      pkgsFor = forAllSystems (system: import nixpkgs {
+        localSystem = system;
+        config.allowUnfree = false;
+      });
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          micromamba-shell.packages.${system}.default
-          pkgs.actionlint
-          pkgs.jq
-          pkgs.moreutils
-          pkgs.markdownlint-cli
-          pkgs.nixfmt-rfc-style
-          pkgs.nodePackages.cspell
-          pkgs.yq
-        ];
+      
+      devShells = forAllSystems (system: 
+        let
+          pkgs = pkgsFor.${system};
+        in 
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              # Accesses the micromamba-shell package for the current system
+              micromamba-shell.packages.${system}.default
+              pkgs.actionlint
+              pkgs.jq
+              pkgs.moreutils
+              pkgs.markdownlint-cli
+              pkgs.nixfmt-rfc-style
+              pkgs.nodePackages.cspell
+              pkgs.yq
+            ];
 
-        shellHook = ''
-          export TMPDIR=$HOME/.tmp
-          export PROD_YAML=${./docker/environment_prod.yaml}
-          export PROD_REQS=${./docker/requirements_prod.txt}
-          export DEV_YAML=${./docker/environment_dev.yaml}
-          export DEV_REQS=${./docker/requirements_dev.txt}
+            shellHook = ''
+              export TMPDIR=$HOME/.tmp
+              export PROD_YAML=${./docker/environment_prod.yaml}
+              export PROD_REQS=${./docker/requirements_prod.txt}
+              export DEV_YAML=${./docker/environment_dev.yaml}
+              export DEV_REQS=${./docker/requirements_dev.txt}
 
-          if [ -z "$MICROMAMBA_EXE" ]; then
-            echo "Entering default micromamba shell..."
-            exec micromamba-shell --rcfile ${./micromamba_shell_hook.sh}
-          fi
-        '';
-      };
+              if [ -z "$MICROMAMBA_EXE" ]; then
+                echo "Entering default micromamba shell..."
+                exec micromamba-shell --rcfile ${./micromamba_shell_hook.sh}
+              fi
+            '';
+          };
+        }
+      );
     };
 }
