0d6ef4babf9f2ae9ff4d8aa26a7ad0d83fd1dc3c
Victoria Neema <victoria@kartoza.com>
2026-02-25 20:52:22 +0300

    Fix overwrite setting

--- src/water_quality/cli/process_annual_wq_variables.py
+++ src/water_quality/cli/process_annual_wq_variables.py
@@ -18,6 +18,7 @@ from odc.stats.model import DateTimeRange
 from water_quality.dates import year_to_dc_datetime
 from water_quality.io import (
     check_directory_exists,
+    check_file_exists,
     get_filesystem,
     get_wq_cog_url,
     get_wq_csv_url,
@@ -238,7 +239,7 @@ def cli(
                     product_version=product_version,
                 )
             )
-            exists = check_directory_exists(expected_stac_path)
+            exists = check_file_exists(expected_stac_path)
             if exists and not overwrite:
                 log.info(
                     f"STAC file already exists for task {task_id_str} at "
